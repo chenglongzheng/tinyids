24ccfc30f70988f716096f3ecdbc7dc5b48c539b
George Notaras <gnot@g-loaded.eu>
2010-03-29 11:05:48 +0300

    Initial code

---
+++ setup.py
@@ -0,0 +1,65 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  This file is part of tinyids.
+#
+#  tinyids - 
+#
+#  Project: https://www.codetrax.org/projects/tinyids
+#
+#  Copyright 2010 George Notaras <gnot [at] g-loaded.eu>, CodeTRAX.org
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#  NOTES
+#
+#  Create source distribution tarball:
+#    python setup.py sdist --formats=gztar
+#
+#  Create binary distribution rpm:
+#    python setup.py bdist --formats=rpm
+#
+#  Create binary distribution rpm with being able to change an option:
+#    python setup.py bdist_rpm --release 7
+#
+#  Test installation:
+#    python setup.py install --prefix=/usr --root=/tmp
+#
+#  Install:
+#    python setup.py install
+#  Or:
+#    python setup.py install --prefix=/usr
+#
+
+
+from distutils.core import setup
+#from setuptools import setup
+from TinyIDS import info
+
+if __name__=='__main__':
+	setup(
+		name = info.name,
+		version = info.version,
+		description = info.description,
+		long_description = info.long_description,
+		author = info.author,
+		author_email = info.author_email,
+		url = info.url,
+		download_url = info.download_url,
+		license = info.license,
+		classifiers = info.classifiers,
+		packages = [
+				'TinyIDS',
+				],
+		scripts = ['scripts/tinyids']
+		)
---
+++ src/TinyIDS/info.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+#
+#  This file is part of tinyids.
+#
+#  tinyids - 
+#
+#  Project: https://www.codetrax.org/projects/tinyids
+#
+#  Copyright 2010 George Notaras <gnot [at] g-loaded.eu>, CodeTRAX.org
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+version = '0.1.0'
+status = 'alpha'
+name = 'tinyids'
+description = """TinyIDS is a ..."""
+long_description = """TinyIDS is a ..."""
+author = 'George Notaras'
+author_email = 'gnot [at] g-loaded.eu'
+url = 'http://www.codetrax.org/projects/tinyids'
+download_url = "http://www.codetrax.org/downloads/projects/" + name + "/" + name + "-" + version + ".tar.gz"
+license = "Apache License version 2"
+
+# Automate the development status for classifiers
+devel_status = ''
+if status == 'pre-alpha':
+	devel_status = 'Development Status :: 2 - Pre-Alpha'
+if status == 'alpha':
+	devel_status = 'Development Status :: 3 - Alpha'
+if status == 'beta':
+	devel_status = 'Development Status :: 4 - Beta'
+if status == 'stable':
+	devel_status = 'Development Status :: 5 - Production/Stable'
+
+# For a list of classifiers check: http://www.python.org/pypi/
+# (http://pypi.python.org/pypi?:action=list_classifiers)
+
+classifiers = [
+	devel_status,
+	'Environment :: Console',
+	'Intended Audience :: System Administrators',
+	'License :: OSI Approved :: Apache Software License',
+	'Natural Language :: English',
+	'Operating System :: OS Independent',
+	'Programming Language :: Python',
+	'Topic :: Utilities',
+	]
+
+def get_version():
+	return name + ' v' + version + '/' + status
+
