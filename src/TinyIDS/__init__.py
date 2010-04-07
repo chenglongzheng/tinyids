d98295906a220c07c9cb7e105dde65694e194303
George Notaras <gnot@g-loaded.eu>
2010-04-07 15:07:33 +0300

    CLOSED #235: Add version information to the TinyIDS package http://www.codetrax.org/issues/show/235

--- src/TinyIDS/__init__.py
+++ src/TinyIDS/__init__.py
@@ -22,3 +22,8 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
+
+from TinyIDS import info
+
+
+version = '%s/%s' % (info.version, info.status)
